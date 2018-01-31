a7ef53d8fabea365c09486485ea42e8df7deea30
Patryk Dolata <patryk.dolata@finanteq.com>
2018-01-31 07:06:01 +0100

    Sort map by object value - refactor

--- src/dolata/patryk/Main.java
+++ src/dolata/patryk/Main.java
@@ -1,11 +1,8 @@
 package dolata.patryk;
 
-import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -18,18 +15,10 @@ public class Main {
         map.put("5", new AlertData("9212", "22212"));
         map.put("6", new AlertData("1203", "22212"));
         Optional<String> maxSerial = getMaxSerialNo(map);
-        if (maxSerial.isPresent()) {
-            System.out.println(maxSerial.get());
-        }
+        maxSerial.ifPresent(System.out::println);
     }
 
     public static Optional<String> getMaxSerialNo(Map<String, AlertData> map) {
-        Map<String, AlertData> sortedNewMap = map.entrySet().stream().sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue,
-                        LinkedHashMap::new));
-        Optional<Map.Entry<String, AlertData>> optional = sortedNewMap.entrySet().stream().findFirst();
-        return optional.map(stringAlertDataEntry -> stringAlertDataEntry.getValue().getId());
+        return map.values().stream().sorted().findFirst().map(AlertData::getId);
     }
 }
