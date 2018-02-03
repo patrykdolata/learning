33b36f1ba5da31298ba0a78c13948b3154084474
Patryk Dolata <patrykdolata@gmail.com>
2018-02-03 16:19:15 +0100

    Vue JS 2 - The Complete Guide - Exercise 3 - solution

--- vue/Exercise 3/app.js
+++ vue/Exercise 3/app.js
@@ -2,5 +2,18 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+        	result: function(){
+          	return this.value != 37 ? 'not there yet': 'done';
+          }
+        },
+        watch: {
+        	result: function(value){
+          var variable = this;
+          	setTimeout(function(){
+          		variable.value = 0;}, 5000);
+          }
+          
         }
     });
