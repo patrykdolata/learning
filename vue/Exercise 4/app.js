c7afe86da2eeb8c4e3b3f1d69c4030f863f21821
Patryk Dolata <patrykdolata@gmail.com>
2018-02-03 20:10:44 +0100

    Vue JS 2 - The Complete Guide - Exercise 4 - solution

--- vue/Exercise 4/app.js
+++ vue/Exercise 4/app.js
@@ -1,11 +1,42 @@
 new Vue({
   el: '#exercise',
   data: {
-
+  	progres: 0,
+  	showCss: false,
+    myClass: {
+    	highlight: false,
+      shrink: true
+    },
+    userClass: 'yellow',
+    isShow: true,
+    userStyle: {
+    	height: '250px',
+      width: '50px',
+      backgroundColor: 'red'
+    },
+    loadingProgres: {
+    	height: '25px',
+      backgroundColor: 'red',
+      width: '0px'
+    }
   },
   methods: {
-    startEffect: function() {
-    
+  	startEffect: function(){
+    	var vm = this;
+    	setInterval(function(){
+      	vm.myClass.highlight = !vm.myClass.highlight;
+        vm.myClass.shrink = !vm.myClass.shrink;
+      },1000);
+    },
+    startProgress: function(){
+    var vm = this;
+    	setInterval(function(){
+      	if(vm.progres < 300){
+      		vm.progres += 15;
+      	}
+        var newWidth = vm.progres + 'px';
+      	vm.loadingProgres.width = newWidth;
+      },1000);
     }
   }
 });
