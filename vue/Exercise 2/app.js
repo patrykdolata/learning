85995104d4e613da5e5725a5b569020c824a9e7e
Patryk Dolata <patrykdolata@gmail.com>
2018-02-03 10:46:07 +0100

    Vue JS 2 - The Complete Guide - Exercise 2 - solution

--- vue/Exercise 2/app.js
+++ vue/Exercise 2/app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+	el: '#exercise',
+  data: {
+  	key: ''
+  },
+  methods: {
+  	buttonClick: function(){
+    	alert("Clicked!")
+    },
+    keyDown: function(event){
+ 			this.key = event.target.value	
+    }
+  }
+})
